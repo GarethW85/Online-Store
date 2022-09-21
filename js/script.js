80e87371772ad9aacb696690de36c8e850ecc410
GarethW85 <tidylad@hotmail.com>
2022-09-21 19:49:03 +0200

    Index page data added with html , css ,  javascript , images and a video
    Added HTML , CSS , JS and images/ videos to the index page.
    
    Sorted the responsive side to the index page so everything is in line.

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// Toggle menu for responsive design
+
+let navLinks = document.getElementById("navLinks");
+    function showMenu() {
+        navLinks.style.right = "0"
+        }
+
+    function hideMenu() {
+        navLinks.style.right = "-200px"
+        } 
+
+
+// Slider
+
+// Sets variables to return inner div and images
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+// Sets variables to return buttons
+const prevBtn = document.querySelector('#prevbutton');
+const nextBtn = document.querySelector('#nextbutton');
+
+// Creates a counter to track which image we are on
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+
+// FUNCTIONS
+
+// Add a function to track next button clicks
+nextBtn.addEventListener('click', function () {
+  if (counter >= carouselImages.length -1) return;
+  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+  counter++;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+//create function to look for PREV button clicks
+prevBtn.addEventListener('click',() => {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+  counter--;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+// This function is triggered after transformation
+carouselSlide.addEventListener('transitionend', () => {
+  if (carouselImages[counter].id === 'lastClone') {
+    carouselSlide.style.transition = 'none';
+  }
+  if (carouselImages[counter].id === 'firstClone') {
+    carouselSlide.style.transition = 'none';
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+});
