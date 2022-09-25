2f2c1548e0cbbb34c9425dab556a2a83f8293188
GarethW85 <tidylad@hotmail.com>
2022-09-25 15:39:30 +0200

    fixed the indentation on my script & cart.js files
    Sorted out the indentation of the code so its more readable .

--- js/cart.js
+++ js/cart.js
@@ -1,147 +1,145 @@
+// Setting up local storage
+class CartItem {
+  constructor(name, img, price) {
+    this.name = name;
+    this.img = img;
+    this.price = price;
+    this.quantity = 1;
+  }
+}
 
 // Setting up local storage
-class CartItem{
-    constructor(name, img, price){
-        this.name = name
-        this.img = img
-        this.price = price
-        this.quantity = 1
+class LocalCart {
+  static key = "cartItems";
+
+  static getLocalCartItems() {
+    let cartMap = new Map();
+    const cart = localStorage.getItem(LocalCart.key);
+    if (cart === null || cart.length === 0) return cartMap;
+    return new Map(Object.entries(JSON.parse(cart)));
+  }
+
+  static addItemToLocalCart(id, item) {
+    let cart = LocalCart.getLocalCartItems();
+    if (cart.has(id)) {
+      let mapItem = cart.get(id);
+      mapItem.quantity += 1;
+      cart.set(id, mapItem);
+    } else cart.set(id, item);
+    localStorage.setItem(
+      LocalCart.key,
+      JSON.stringify(Object.fromEntries(cart))
+    );
+    updateCartUI();
+  }
+
+  static removeItemFromCart(id) {
+    let cart = LocalCart.getLocalCartItems();
+    if (cart.has(id)) {
+      let mapItem = cart.get(id);
+      if (mapItem.quantity > 1) {
+        mapItem.quantity -= 1;
+        cart.set(id, mapItem);
+      } else cart.delete(id);
     }
+    if (cart.length === 0) localStorage.clear();
+    else
+      localStorage.setItem(
+        LocalCart.key,
+        JSON.stringify(Object.fromEntries(cart))
+      );
+    updateCartUI();
+  }
 }
-
-// Setting up local storage
-class LocalCart{
-    static key = 'cartItems'
-
-    static getLocalCartItems(){
-        let cartMap = new Map()
-        const cart = localStorage.getItem(LocalCart.key)
-        if(cart===null || cart.length===0) return cartMap
-          return new Map(Object.entries(JSON.parse(cart)))
-        }
-
-    static addItemToLocalCart(id, item){
-        let cart = LocalCart.getLocalCartItems()
-        if(cart.has(id)){
-            let mapItem = cart.get(id)
-            mapItem.quantity +=1
-            cart.set(id, mapItem)
-        }
-        else 
-        cart.set(id, item)
-        localStorage.setItem(LocalCart.key, JSON.stringify(Object.fromEntries(cart)))
-        updateCartUI()
-        }
-
-    static removeItemFromCart(id){
-        let cart = LocalCart.getLocalCartItems()
-        if(cart.has(id)){
-            let mapItem = cart.get(id)
-            if(mapItem.quantity>1)
-            {
-                mapItem.quantity -=1
-                cart.set(id, mapItem)
-            }
-            else 
-            cart.delete(id)
-        }
-            if (cart.length===0)
-            localStorage.clear()
-            else
-            localStorage.setItem(LocalCart.key, JSON.stringify(Object.fromEntries(cart)))
-            updateCartUI()
-        }
-}
-
 
 // Display cart on hover and hide cart when cursor is away from body
-const cartIcon = document.querySelector('.bi-bag')
-const wholeCartWindow =  document.querySelector('.whole-cart-window') 
-wholeCartWindow.inWindow = 0
-const addToCartBtns = document.querySelectorAll('.add-cart')
-addToCartBtns.forEach((btn) =>{
-    btn.addEventListener('click', addItemFunction)
-} )
+const cartIcon = document.querySelector(".bi-bag");
+const wholeCartWindow = document.querySelector(".whole-cart-window");
+wholeCartWindow.inWindow = 0;
+const addToCartBtns = document.querySelectorAll(".add-cart");
+addToCartBtns.forEach((btn) => {
+  btn.addEventListener("click", addItemFunction);
+});
 
-
-function addItemFunction(e){
-    const id = e.target.parentElement.parentElement.parentElement.getAttribute('data-id')
-    const img = e.target.parentElement.parentElement.previousElementSibling.src
-    const name = e.target.parentElement.previousElementSibling.textContent
-    const price = e.target.parentElement.children[0].textContent
-    // price = price.replace("Price: $", '')
-    const item = new CartItem (name, img, price)
-    LocalCart.addItemToLocalCart(id, item)
-    console.log(price)
+// Add item to cart
+function addItemFunction(e) {
+  const id = e.target.parentElement.parentElement.parentElement.getAttribute("data-id");
+  const img = e.target.parentElement.parentElement.previousElementSibling.src;
+  const name = e.target.parentElement.previousElementSibling.textContent;
+  let price = e.target.parentElement.children[0].textContent;
+  // price = price.replace("Price: R", '')
+  const item = new CartItem(name, img, price);
+  LocalCart.addItemToLocalCart(id, item);
+  alert("Item added to cart");
+  console.log(price);
 }
 
+// Event listeners for mouse movement over cart area icon
+cartIcon.addEventListener("mouseover", () => {
+  if (wholeCartWindow.classList.contains("hide"))
+    wholeCartWindow.classList.remove("hide");
+});
 
-cartIcon.addEventListener('mouseover', ()=>{
-    if(wholeCartWindow.classList.contains('hide'))
-    wholeCartWindow.classList.remove('hide')
-})
+cartIcon.addEventListener("mouseleave", () => {
+  // if(wholeCartWindow.classList.contains('hide'))
+  setTimeout(() => {
+    if (wholeCartWindow.inWindow === 0) {
+      wholeCartWindow.classList.add("hide");
+    }
+  }, 500);
+});
 
-cartIcon.addEventListener('mouseleave', ()=>{
-    // if(wholeCartWindow.classList.contains('hide'))
-    setTimeout( () =>{
-        if (wholeCartWindow.inWindow===0){
-            wholeCartWindow.classList.add('hide')
-        }
-    } , 500 )
-})
+wholeCartWindow.addEventListener("mouseover", () => {
+  wholeCartWindow.inWindow = 1;
+});
 
-wholeCartWindow.addEventListener('mouseover', ()=>{
-    wholeCartWindow.inWindow = 1
-})
-
-wholeCartWindow.addEventListener('mouseleave', ()=>{
-    wholeCartWindow.classList.add('hide')
-    wholeCartWindow.inWindow = 0
-})
+wholeCartWindow.addEventListener("mouseleave", () => {
+  wholeCartWindow.classList.add("hide");
+  wholeCartWindow.inWindow = 0;
+});
 
 // Displays the carts UI once new items are added
-function updateCartUI(){
-    const cartWrapper = document.querySelector('.cart-wrapper')
-    cartWrapper.innerHTML = ""
-    const items = LocalCart.getLocalCartItems()
-    if(items === null) return
-    let count = 0
-    let total = 0
-    for(const [key, value] of items.entries()){
-        const cartItem = document.createElement('div')
-        cartItem.classList.add('cart-item')
-        let price = value.price*value.quantity
-        price = Math.round(price*100)/100
-        count+=1
-        total += price
-        total = Math.round(total*100)/100
-        cartItem.innerHTML = `
+function updateCartUI() {
+  const cartWrapper = document.querySelector(".cart-wrapper");
+  cartWrapper.innerHTML = "";
+  const items = LocalCart.getLocalCartItems();
+  if (items === null) return;
+  let count = 0;
+  let total = 0;
+  for (const [key, value] of items.entries()) {
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("cart-item");
+    let price = value.price * value.quantity;
+    price = Math.round(price * 100) / 100;
+    count += 1;
+    total += price;
+    total = Math.round(total * 100) / 100;
+    cartItem.innerHTML = `
                              <img src="${value.img}" alt="">
                                 <div class="details">
                                 <h3>${value.name}</h3>
                                     <p>
                                         <span class="quantity">${value.quantity}</span>
-                                        <span class="price">Price: R${price}</span>
+                                        <span class="price">${price}</span>
                                     </p>
                                     </div>
                                 <div class="cancel"><i class="bi bi-x-circle-fill"></i></div>
-    `
+    `;
+    cartItem.lastElementChild.addEventListener("click", () => {
+      LocalCart.removeItemFromCart(key);
+    });
+    cartWrapper.append(cartItem);
+  }
 
-    cartItem.lastElementChild.addEventListener('click', ()=>{
-        LocalCart.removeItemFromCart(key)
-    })
-    cartWrapper.append(cartItem)
-    }
-
-    if(count > 0){
-        cartIcon.classList.add('non-empty')
-        let root = document.querySelector(':root')
-        root.style.setProperty('--after-content', `"${count}"`)
-        const subtotal = document.querySelector('.subtotal')
-        subtotal.innerHTML = `SubTotal: R${total}`
-    } 
-    else 
-    cartIcon.classList.remove('non-empty')
+  if (count > 0) {
+    cartIcon.classList.add("non-empty");
+    let root = document.querySelector(":root");
+    root.style.setProperty("--after-content", `"${count}"`);
+    const subtotal = document.querySelector(".subtotal");
+    subtotal.innerHTML = `SubTotal: ${total}`;
+  } else cartIcon.classList.remove("non-empty");
 }
 
-document.addEventListener('DOMContentLoaded', ()=>{updateCartUI()})
+document.addEventListener("DOMContentLoaded", () => {
+  updateCartUI();
+});
--- js/script.js
+++ js/script.js
@@ -1,51 +1,50 @@
 // Toggle menu for responsive design
 let navLinks = document.getElementById("navLinks");
-    function showMenu() {
-        navLinks.style.right = "0"
-        }
+function showMenu() {
+  navLinks.style.right = "0";
+}
 
-    function hideMenu() {
-        navLinks.style.right = "-200px"
-        } 
-
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
 
 // Slider
 // Sets variables to return inner div and images
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
-const prevBtn = document.querySelector('#prevbutton');
-const nextBtn = document.querySelector('#nextbutton');
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+const prevBtn = document.querySelector("#prevbutton");
+const nextBtn = document.querySelector("#nextbutton");
 // Creates a counter to track which image we are on
 let counter = 1;
 const size = carouselImages[0].clientWidth;
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+
 // FUNCTIONS
 // Add a function to track next button clicks
-nextBtn.addEventListener('click', function () {
-  if (counter >= carouselImages.length -1) return;
-  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+nextBtn.addEventListener("click", function () {
+  if (counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter++;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
 //create function to look for PREV button clicks
-prevBtn.addEventListener('click',() => {
+prevBtn.addEventListener("click", () => {
   if (counter <= 0) return;
-  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter--;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
 // This function is triggered after transformation
-carouselSlide.addEventListener('transitionend', () => {
-  if (carouselImages[counter].id === 'lastClone') {
-    carouselSlide.style.transition = 'none';
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[counter].id === "lastClone") {
+    carouselSlide.style.transition = "none";
   }
-  if (carouselImages[counter].id === 'firstClone') {
-    carouselSlide.style.transition = 'none';
+  if (carouselImages[counter].id === "firstClone") {
+    carouselSlide.style.transition = "none";
     counter = carouselImages.length - counter;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
   }
 });
-
 
 // Scrolling Effects
 function reveal() {
@@ -66,8 +65,8 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
-// Email sent 
-function emailsent () {
+// Email sent
+function emailsent() {
   alert("Email recevied, we will be in touch");
   window.location.href = "index.html";
 }
